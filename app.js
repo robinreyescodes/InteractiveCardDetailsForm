9f6c49556957deed9a066a033d50fecda6d0845e
Robin Reyes <robinreyes.code@gmail.com>
2022-08-31 14:53:56 -0700

    card values change as user types now still things to fix

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const yearInput = document.querySelector("#year");
 const cvcInput = document.querySelector("#cvc");
 const cardUser = document.querySelector("#card-user");
 const cardNumber = document.querySelector("#card-number");
-const cardExpiration = document.querySelector("#card-expiration");
+const monthExpiration = document.querySelector("#month-span");
+const yearExpiration = document.querySelector("#year-span");
 const cardCvc = document.querySelector("#card-back-number");
 
 nameInput.addEventListener("keyup", (e) => {
@@ -13,7 +14,15 @@ nameInput.addEventListener("keyup", (e) => {
 });
 
 numberInput.addEventListener("keyup", (e) => {
+  monthInput.value = "";
   cardNumber.textContent = numberInput.value;
+});
+
+monthInput.addEventListener("keyup", (e) => {
+  monthExpiration.textContent = monthInput.value;
+});
+yearInput.addEventListener("keyup", (e) => {
+  yearExpiration.textContent = yearInput.value;
 });
 
 cvcInput.addEventListener("keyup", (e) => {
